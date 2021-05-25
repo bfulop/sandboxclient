e26580f50d99f34d404cc4fa16155dae4efa1de1
bfulop <bfulop@mac.com>
2021-05-25 14:14:34 +0200

    Testing git deploy

--- api/hello.ts
+++ api/hello.ts
@@ -2,5 +2,5 @@ import { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default (request: VercelRequest, response: VercelResponse): void => {
   const { name = 'World' } = request.query
-  response.status(200).send(`Hello ${name}!`)
+  response.status(200).send(`Hello dear ${name}!`)
 }
